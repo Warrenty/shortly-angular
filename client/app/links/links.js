9bc77c35d5c6fc6928a915d5829d34bc9fe6b64a
warrenty <warranty6@gmail.com>
2014-10-10 15:48:12 -0700

    Passing all specs

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,14 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $http) {
   // Your code here
+  console.log("TESTTTTTT", Links);
+  $scope.data = {};
+  $scope.getLinks = function(){
+    $http.get('/api/links').
+      success(function(data, status, headers, config) {
+        $scope.data.links = data;
+    });
+  }
+  $scope.getLinks();
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,11 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, $http) {
   // Your code here
+  $scope.link = {};
+  $scope.addLink = function(){
+    $http.post('/api/links').
+    success(function(data, status, headers, config){
+    })
+  };
 });
